4585e44b53de2f81d47c60a3fb513f87eb6ca8f7
Nathan Fulkerson <4764329+NFulkerson@users.noreply.github.com>
2018-06-20 15:54:13 -0400

    Add default codable conformance to Movie type Movie model no longer holds references to UIImage, only a URL resource for the image

--- Movie Night/API/Models/Movie.swift
+++ Movie Night/API/Models/Movie.swift
@@ -8,84 +8,76 @@
 
 import UIKit
 
-enum ImageState {
-    case placeholder
-    case downloaded
-    case failed
-}
+class Movie: Codable {
+  let id: Int
 
-class Movie: Decodable {
-    var imageState: ImageState = .placeholder
-    let id: Int
+  let backdropPath: String
+  let belongsToCollection: Bool?
+//  let genres: [Genre]
 
-    let backdropPath: String
-    let belongsToCollection: Bool?
-    let genres: [Genre]
+  let overview: String?
+  let popularity: Double
+  let posterPath: String?
 
-    let overview: String?
-    let popularity: Double
-    let posterPath: String?
+  let releaseDate: String
+  let title: String
 
-    let releaseDate: String
-    let runtime: Int
+  let voteAverage: Double
+  let voteCount: Int
 
-    let tagline: String
-    let title: String
-
-    let voteAverage: Double
-    let voteCount: Int
-
-    var image: UIImage?
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case backdropPath
-        case belongsToCollection
-        case genres
-        case overview
-        case popularity
-        case posterPath
-        case releaseDate
-        case runtime
-        case tagline
-        case title
-        case voteAverage
-        case voteCount
+  var imageURLPath: URL? {
+    guard let path = posterPath else {
+      return nil
     }
+    return URL(string: "https://image.tmdb.org/t/p/w185/\(path)")
+  }
+
+  private enum CodingKeys: String, CodingKey {
+    case id
+    case backdropPath
+    case belongsToCollection
+    case overview
+    case popularity
+    case posterPath
+    case releaseDate
+    case title
+    case voteAverage
+    case voteCount
+  }
 
 }
 
 // MARK: Movie-related endpoints
 
 struct GetMovieDetails: APIRequest {
-    typealias Response = Movie
+  typealias Response = Movie
 
-    var resourceName: String {
-        return "movie/\(movieId)"
-    }
+  var resourceName: String {
+    return "movie/\(movieId)"
+  }
 
-    var parameters: [URLQueryItem] = []
+  var parameters: [URLQueryItem] = []
 
-    let movieId: Int
+  let movieId: Int
 
-    init(id: Int) {
-        movieId = id
-    }
+  init(id: Int) {
+    movieId = id
+  }
 }
 
 struct GetPopularMovies: APIRequest {
-    typealias Response = ListContainer
+  typealias Response = ListContainer
 
-    var resourceName: String {
-        return "movie/popular"
-    }
+  var resourceName: String {
+    return "movie/popular"
+  }
 
-    var parameters: [URLQueryItem] = []
+  var parameters: [URLQueryItem] = []
 }
 
 struct ListContainer: Decodable {
-    let page: Int
-    let totalPages: Int
-    let totalResults: Int
-    let results: [Movie]
+  let page: Int
+  let totalPages: Int
+  let totalResults: Int
+  let results: [Movie]
 }
